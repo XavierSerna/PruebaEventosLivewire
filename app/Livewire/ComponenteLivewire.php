c686cdc1cda3a19f41944d77d835ff743617d8e0
xavier <xavierserna472@gmail.com>
2024-01-18 16:53:03 -0500

    Más botones, uno para disminuir, y una area de botones distintos que agregan el nombre/ tipo del boton (text, textarea, checkbox) a un nuevo array

--- app/Livewire/ComponenteLivewire.php
+++ app/Livewire/ComponenteLivewire.php
@@ -7,14 +7,26 @@ use Livewire\Component;
 class ComponenteLivewire extends Component
 {
     public $contador = 0;
-
     public $listaItems = [];
+    public $listaBotones = [];
 
     public function incrementar()
     {
         $this->contador++;
         $this->listaItems[] = $this->contador;
         $this->dispatch('contador-cambiado', $this->contador);
+    }
+
+    public function disminuir()
+    {
+        $this->contador--;
+        $this->listaItems[] = $this->contador;
+        $this->dispatch('contador-cambiado', $this->contador);
+    }
+
+    public function listar($type)
+    {
+        $this->listaBotones[] = $type;
     }
 
     public function render()
--- resources/views/livewire/componente-livewire.blade.php
+++ resources/views/livewire/componente-livewire.blade.php
@@ -1,11 +1,28 @@
 <div>
     {{-- Care about people's approval and you will be their prisoner. --}}
     <button wire:click="incrementar">Incrementar</button>
+    <button wire:click="disminuir">Disminuir</button>
     <h1>{{ $contador }}</h1>
     <div>
         <ul>
             @foreach($listaItems as $item)
             <li>{{ $item }}</li>
+            @endforeach
+        </ul>
+    </div>
+    <div>
+        @foreach(['text'=>'Text', 'textarea'=>'Textarea', 'checkbox'=>'Checkbox'] as $type=>$label)
+        <button wire:click="listar('{{ $type }}')" class="btn btn-light" data-type="{{ $type }}">
+            {{ $label }}
+        </button>
+        @endforeach
+    </div>
+    <div class="card" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+            @foreach($listaBotones as $boton)
+            <li class="list-group-item">
+                {{ $boton }}
+            </li>
             @endforeach
         </ul>
     </div>
